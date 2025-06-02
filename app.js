f628ada175bb36b7e2aee3a6cee47d9ca38d5a63
luisdiego99 <125165264+luisdiego99@users.noreply.github.com>
2025-06-02 00:38:49 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// MongoDB Setup
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/desarrolloWeb';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('MongoDB connected successfully');
+})
+.catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var taskRouter = require('./routes/tasks');
+var goalsRouter = require('./routes/goals');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Auth middleware
+app.use((req, res, next) => {
+  console.log("Auth check for:", req.url);
+  if (req.headers.authorization && req.headers.authorization === "123") {
+    return next();
+  }
+  console.log("Blocking unauthorized access to:", req.url);
+  res.status(401).json({ message: "Unauthorized" });
+});
+
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/tasks', taskRouter);
+app.use('/goals', goalsRouter);
+
+// Error handlers
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
