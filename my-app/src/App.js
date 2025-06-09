ea310b15f4a4b74d4c3895438cdfb03aaa35a01e
luisdiego99 <125165264+luisdiego99@users.noreply.github.com>
2025-06-08 23:42:10 -0600

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,51 +8,37 @@ import Formulary from './Components/Form.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addTodo, initAddTodo, todoSlice } from './reducers/todoSlice.js';
+import { fetchGoalsAsync } from './reducers/todoSlice.js';
 
 function App() {
   const dispatch = useDispatch();
-  const todos = useSelector((state) => state.todos.value);
-  const arr = [
-    {
-      'name':'Tender la cama',
-      'description': 'Extender el edredón y ordenar las almohadas.',
-      'date': '05/05/2025'
-    },
-    {
-      'name':'Preparar café',
-      'description': 'Preparar un café con 15g de café y 400g de agua. Lavar los utensilios.',
-      'date': '06/05/2025'
-    }
-  ]
+  const todos = useSelector((state) => state.todos.items || []); // Add this line
+  const status = useSelector((state) => state.todos.status);
 
   useEffect(() => {
-    arr.map((item)=> {
-      dispatch(initAddTodo(item))
-    })
-  }, []);
+    dispatch(fetchGoalsAsync());
+  }, [dispatch]);
 
   return (
     <div className="App">
-           <Menu></Menu>
+      <Menu></Menu>
       <Container>
         <Row> 
-        <Col><Formulary></Formulary></Col>
-        <Col>
-        {
-          todos.map((todo,index) => {
-            return (
-              <Item key={index} name={todo.name} description={todo.description} date={todo.date}/>
-            )
-          })
-        }
-
-        </Col>
+          <Col><Formulary></Formulary></Col>
+          <Col>
+            {status === 'loading' && <p>Loading...</p>}
+            {todos.map((todo, index) => (
+              <Item 
+                key={todo._id || index}  // Better to use todo._id if available
+                _id={todo._id}
+                name={todo.name} 
+                description={todo.description} 
+                date={todo.dueDate || todo.date}  // Match your data structure
+              />
+            ))}
+          </Col>
         </Row>
-        
       </Container>
- 
-      
     </div>
   );
 }
