57a047b711b9d0a3ae997425dbffed1078bb00e8
luisdiego99 <125165264+luisdiego99@users.noreply.github.com>
2025-06-08 23:39:54 -0600

    Update Form.js

--- my-app/src/Components/Form.js
+++ my-app/src/Components/Form.js
@@ -1,12 +1,9 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import './Form.scss'
-import { useSelector,useDispatch } from 'react-redux';
+import './Form.scss';
 import { useRef } from 'react';
-import {
-  removeTodo,
-  addTodo
-} from '../reducers/todoSlice.js';
+import { useDispatch } from 'react-redux';
+import { addGoalAsync } from '../reducers/todoSlice';
 
 function Formulario() {
   const dispatch = useDispatch();
@@ -14,34 +11,68 @@ function Formulario() {
   const inputRefDescription = useRef();
   const inputRefDueDate = useRef();
 
-  const addItem = (e) => {
+  const addItem = async (e) => {
     e.preventDefault();
-    dispatch(addTodo({
-      'name': inputRefName.current.value,
-      'description': inputRefDescription.current.value,
-      'date': inputRefDueDate.current.value
-    }))
-  }
+    
+    // Validate inputs
+    if (!inputRefName.current.value || 
+        !inputRefDescription.current.value || 
+        !inputRefDueDate.current.value) {
+      alert('Please fill all fields');
+      return;
+    }
+
+    try {
+      await dispatch(addGoalAsync({
+        name: inputRefName.current.value,
+        description: inputRefDescription.current.value,
+        dueDate: inputRefDueDate.current.value
+      })).unwrap();
+      
+      // Clear form after successful submission
+      inputRefName.current.value = '';
+      inputRefDescription.current.value = '';
+      inputRefDueDate.current.value = '';
+    } catch (error) {
+      console.error('Failed to save goal:', error);
+      alert('Failed to save goal. Please try again.');
+    }
+  };
 
   return (
-  <div className='space'>
-       <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="" ref={inputRefName}/>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Description</Form.Label>
-        <Form.Control as="textarea" rows={3} type="text" placeholder="" ref={inputRefDescription} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Due Date</Form.Label>
-        <Form.Control type="date" ref={inputRefDueDate}/>
-      </Form.Group>
-      <Button variant="info" onClick={addItem}>Add Goal</Button>
-    </Form>
-  </div>
- 
+    <div className='space'>
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="" 
+            ref={inputRefName}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control 
+            as="textarea" 
+            rows={3} 
+            type="text" 
+            placeholder="" 
+            ref={inputRefDescription}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Due Date</Form.Label>
+          <Form.Control 
+            type="date" 
+            ref={inputRefDueDate}
+            required
+          />
+        </Form.Group>
+        <Button variant="info" onClick={addItem}>Add Goal</Button>
+      </Form>
+    </div>
   );
 }
 
